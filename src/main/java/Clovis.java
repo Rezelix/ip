452b4283fa9bd31ab349d84aa1a4045723da2de6
Rezelix <rezelixvolixant@gmail.com>
2025-08-26 23:17:25 +0800

    convert Duke to Clovis, the egotistic AI

---
+++ src/main/java/Clovis.java
@@ -0,0 +1,15 @@
+public class Clovis {
+    public static void main(String[] args) {
+        String logo = "  _____ _            _     \n" +
+                " / ____| |          (_)    \n" +
+                "| |    | | _____   ___ ___ \n" +
+                "| |    | |/ _ \\ \\ / / / __|\n" +
+                "| |____| | (_) \\ V /| \\__ \\\n" +
+                " \\_____|_|\\___/ \\_/ |_|___/";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What do you want from me this time?");
+        System.out.println("__________________________________________________________");
+        System.out.println("Bye. Don't come again!");
+        System.out.println("__________________________________________________________");
+    }
+}
