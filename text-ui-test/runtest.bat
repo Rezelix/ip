b9541711f8b3ae6f6a03805dad0d86b8eb6836ac
Rezelix <rezelixvolixant@gmail.com>
2025-09-23 23:58:43 +0800

    edit runtest bat to involve all java files

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\clovis\Clovis.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\clovis\*.java ..\src\main\java\clovis\task\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
